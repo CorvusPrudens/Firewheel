18fd34131de32acc1f1c2a9acc82779b0b890dce
Adam Pescoller <adam.pescoller@gmail.com>
2025-06-12 01:23:54 +0200

    made filter steepness constants public

--- crates/firewheel-core/src/dsp/filter/primitives/spec.rs
+++ crates/firewheel-core/src/dsp/filter/primitives/spec.rs
@@ -1,34 +1,34 @@
 pub type FilterOrder = usize;
 
 /// Filter order achieving a steepness of 6 dB/oct
-const DB_OCT_6: usize = 1;
+pub const DB_OCT_6: usize = 1;
 /// Filter order achieving a steepness of 12 dB/oct
-const DB_OCT_12: usize = 2;
+pub const DB_OCT_12: usize = 2;
 /// Filter order achieving a steepness of 18 dB/oct
-const DB_OCT_18: usize = 3;
+pub const DB_OCT_18: usize = 3;
 /// Filter order achieving a steepness of 24 dB/oct
-const DB_OCT_24: usize = 4;
+pub const DB_OCT_24: usize = 4;
 /// Filter order achieving a steepness of 30 dB/oct
-const DB_OCT_30: usize = 5;
+pub const DB_OCT_30: usize = 5;
 /// Filter order achieving a steepness of 36 dB/oct
-const DB_OCT_36: usize = 6;
+pub const DB_OCT_36: usize = 6;
 /// Filter order achieving a steepness of 42 dB/oct
-const DB_OCT_42: usize = 7;
+pub const DB_OCT_42: usize = 7;
 /// Filter order achieving a steepness of 48 dB/oct
-const DB_OCT_48: usize = 8;
+pub const DB_OCT_48: usize = 8;
 /// Filter order achieving a steepness of 54 dB/oct
-const DB_OCT_54: usize = 9;
+pub const DB_OCT_54: usize = 9;
 /// Filter order achieving a steepness of 60 dB/oct
-const DB_OCT_60: usize = 10;
+pub const DB_OCT_60: usize = 10;
 /// Filter order achieving a steepness of 66 dB/oct
-const DB_OCT_66: usize = 11;
+pub const DB_OCT_66: usize = 11;
 /// Filter order achieving a steepness of 72 dB/oct
-const DB_OCT_72: usize = 12;
+pub const DB_OCT_72: usize = 12;
 /// Filter order achieving a steepness of 78 dB/oct
-const DB_OCT_78: usize = 13;
+pub const DB_OCT_78: usize = 13;
 /// Filter order achieving a steepness of 84 dB/oct
-const DB_OCT_84: usize = 14;
+pub const DB_OCT_84: usize = 14;
 /// Filter order achieving a steepness of 90 dB/oct
-const DB_OCT_90: usize = 15;
+pub const DB_OCT_90: usize = 15;
 /// Filter order achieving a steepness of 96 dB/oct
-const DB_OCT_96: usize = 16;
+pub const DB_OCT_96: usize = 16;
