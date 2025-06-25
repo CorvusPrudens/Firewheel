0693ba5f4c105875788fe59622d1ec6e6fd59805
Adam Pescoller <adam.pescoller@gmail.com>
2025-06-25 20:03:21 +0200

    made default UI window larger

--- examples/visual_node_graph/src/main.rs
+++ examples/visual_node_graph/src/main.rs
@@ -11,7 +11,7 @@ fn main() -> eframe::Result<()> {
 
     let native_options = eframe::NativeOptions {
         viewport: egui::ViewportBuilder::default()
-            .with_inner_size([900.0, 600.0])
+            .with_inner_size([1200.0, 800.0])
             .with_min_inner_size([300.0, 220.0]),
         vsync: true,
         ..Default::default()
